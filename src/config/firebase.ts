4fe8eb8c036d3755d416682d6a473c630f8352bc
Lucas Matheus <89390605+Wuzuy@users.noreply.github.com>
2026-05-14 10:30:21 -0300

    V0.1.0
    Removendo chaves sensíveis e configurando .env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Utiliza variáveis de ambiente do Vite
 const firebaseConfig = {
-  apiKey: "AIzaSyB1UY11uxSjgWCxAdD11ewukeBqpWTRqXU",
-  authDomain: "senai-wuzuy.firebaseapp.com",
-  projectId: "senai-wuzuy",
-  storageBucket: "senai-wuzuy.firebasestorage.app",
-  messagingSenderId: "124079474870",
-  appId: "1:124079474870:web:cb31e719db890e12fa7e17",
-  measurementId: "G-MYKWHCM1PH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
